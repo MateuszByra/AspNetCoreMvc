42e4205aada9e663ac77ba9b33a21ed04d0940ae
MateuszByra <mat.byra@gmail.com>
2017-04-30 10:53:11 +0200

    Add AccountController. Create register view with register form. Create RegisterUserViewModel which was used in register view.

---
+++ OdeToFood/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.ViewModels;
+
+namespace OdeToFood.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterUserViewModel model)
+        {
+            return null;
+        }
+    }
+}
